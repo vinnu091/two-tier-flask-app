2a21100ff637a06c2a62c2f4b8dd055d39faaa1e
Shy108 <159191430+Shy108@users.noreply.github.com>
2025-11-08 14:25:03 +0100

    Update wait-for-mysql.sh
    delete wait-for-mysql.sh

--- templates/wait-for-mysql.sh
+++ templates/wait-for-mysql.sh
@@ -1,12 +1,1 @@
-#!/bin/sh
 
-set -e
-
-echo "Waiting for MySQL to be ready..."
-until mysql -h "$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW DATABASES;" >/dev/null 2>&1; do
-  echo "MySQL is not ready yet..."
-  sleep 3
-done
-
-echo "MySQL is up! Starting Flask..."
-exec python app.py
